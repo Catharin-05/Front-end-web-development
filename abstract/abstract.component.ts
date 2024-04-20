3cfa0f789db8d7a3a1972068ca1286ba3d53540a
Catharin-05 <124374895+Catharin-05@users.noreply.github.com>
2024-04-20 20:44:13 +0530

    Add files via upload
    This is the home page for the cathart website using Angular.

---
+++ abstract/abstract.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abstract',
+  standalone: true,
+  imports: [],
+  templateUrl: './abstract.component.html',
+  styleUrl: './abstract.component.css'
+})
+export class AbstractComponent {
+
+}
---
+++ landscapes/landscapes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landscapes',
+  standalone: true,
+  imports: [],
+  templateUrl: './landscapes.component.html',
+  styleUrl: './landscapes.component.css'
+})
+export class LandscapesComponent {
+
+}
